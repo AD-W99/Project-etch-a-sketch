a429ab19b8d9b37561d94d1b9c45cb2d4e7b9c15
Alexander <alex.wood1722@gmail.com>
2022-01-15 09:44:04 -0800

    Modified sketchpad.html sketchpad-logic.js sketchpad-styles.css

--- sketchpad-logic.js
+++ sketchpad-logic.js
@@ -1,14 +1,46 @@
 /* https://stackoverflow.com/questions/57546807/how-to-pass-in-a-variable-to-the-css-grid-repeat-function */
 
 const gridContainer = document.querySelector('.grid-container');
+const clearButton = document.querySelector('.clearButton');
+let gridDimensions = 16;
 
-function onStartUp() {
-    for (i = 0; i < 256; i++) {
+function makeGrid() {
+    for (i = 0; i < (gridDimensions ** 2); i++) {
         div = document.createElement('div');
         div.style.backgroundColor = "grey";
-        div.style.border = "1px solid black";
+        div.classList.add('square');
         gridContainer.appendChild(div);
+        gridContainer.addEventListener('mouseover', function(e){
+            e.target.style.backgroundColor = "black";
+        });
+        gridContainer.style.gridTemplate = `repeat(${gridDimensions}, 1fr) / repeat(${gridDimensions}, 1fr)`;
     }
 }
 
-onStartUp();
+makeGrid();
+
+let divList = document.getElementsByClassName('square');
+
+clearButton.addEventListener('click',() => {
+    for (i = 0; i < divList.length; i++) {
+        divList[i].style.backgroundColor = "grey";
+    }
+});
+
+let slider = document.querySelector('.gridValueButton');
+let sliderDisplay = document.querySelector('.gridValueDisplay');
+
+sliderDisplay.textContent = `${slider.value}x${slider.value}`;
+slider.oninput = () => {
+    sliderDisplay.textContent = `${slider.value}x${slider.value}`;
+    gridDimensions = slider.value;
+}
+
+let remakeGridButton = document.querySelector('.remakeGrid');
+
+remakeGridButton.addEventListener('click', () => {
+    for (i = 0; i < divList.length; i++) {
+        divList[i].style.backgroundColor = "grey";
+    }
+    makeGrid();
+});
